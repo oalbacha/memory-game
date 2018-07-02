1ab7a8f2054b392214aa32bf61d707be3d0711f1
Omar Albacha <oalbacha@gmail.com>
2018-07-02 13:42:13 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//Start by building a very simple grid of cards.
+//Don't worry about styling, just get something clickable on the page.
+//Figure out the HTML needed to represent a card. Remember, you have to represent two sides of the card. Are you going to have two //separate elements stacked on top of each other?
+//Add the functionality to handle clicks.
+//This should reveal the hidden side of each card.
+//Work on the matching logic. How does your game "know" if a player guesses correctly or incorrectly?
+//Work on the winning condition. How does your game “know” if a player has won?
+//We recommend saving styling until the very end. Allow your game logic and functionality to dictate the styling.
+
+//Get the card to flip on click
+//flip another card 
+//Add comparison logic between the two flipped cards. if they match they stay flipped and if the don’t they reset to unflipped
